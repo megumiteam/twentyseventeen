c5a31c2135bb16068a80e3126201a27bb1524080
Kazue Igarashi <igarashi@digitalcube.jp>
2019-08-28 18:20:32 +0900

    Change Custom Header image.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -16,7 +16,7 @@
  */
 function twentyseventeen_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'twentyseventeen_custom_header_args', array(
-		'default-image'      => get_parent_theme_file_uri( '/assets/images/header.jpg' ),
+		'default-image'      => get_parent_theme_file_uri( '/assets/images/shifter.png' ),
 		'default-text-color' => 'ffffff',
 		'width'              => 2000,
 		'height'             => 1200,
@@ -26,8 +26,8 @@ function twentyseventeen_custom_header_setup() {
 
 	register_default_headers( array(
 		'default-image' => array(
-			'url'           => '%s/assets/images/header.jpg',
-			'thumbnail_url' => '%s/assets/images/header.jpg',
+			'url'           => '%s/assets/images/shifter.png',
+			'thumbnail_url' => '%s/assets/images/shifter.png',
 			'description'   => __( 'Default Header Image', 'twentyseventeen' ),
 		),
 	) );
